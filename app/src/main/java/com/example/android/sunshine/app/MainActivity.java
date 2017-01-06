3bcc5722d2b896645c8f98585e41aff12d015db7
Ola <ola466@gmail.com>
2017-01-05 23:20:07 -0500

    Fix maps and share action

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -60,7 +60,10 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         }
-
+        if (id == R.id.action_map) {
+            openPreferredLocationInMap();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
